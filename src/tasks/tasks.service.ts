bc4297f678fec77aa196c9416c92e328a485a6b7
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-03-18 18:37:46 -0300

    feat: ensure TasksController createTask calls TasksService with correct values

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +8,13 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  createTask(task: Task) {
+    const createTask: Task = { ...task };
+    createTask.status = TaskStatus.OPEN;
+    createTask.id = uuid();
+    this.tasks.push(task);
+    return createTask;
   }
 }
