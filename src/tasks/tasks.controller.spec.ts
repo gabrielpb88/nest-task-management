5330f23dfcd2855935a55d5d3133b5537b873be7
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-03-18 18:14:09 -0300

    test: ensure TasksController calls TasksService correctly

--- src/tasks/tasks.controller.spec.ts
+++ src/tasks/tasks.controller.spec.ts
@@ -1,18 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TasksController } from './tasks.controller';
+import spyOn = jest.spyOn;
+import { TasksService } from './tasks.service';
 
 describe('TasksController', () => {
   let controller: TasksController;
+  let service: TasksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TasksController],
+      providers: [TasksService],
     }).compile();
 
     controller = module.get<TasksController>(TasksController);
+    service = module.get<TasksService>(TasksService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call service getAllTasks when getAllTasks', () => {
+    const spyGetAllTasks = jest.spyOn(service, 'getAllTasks');
+    controller.getAllTasks();
+    expect(spyGetAllTasks).toBeCalledTimes(1);
   });
 });
