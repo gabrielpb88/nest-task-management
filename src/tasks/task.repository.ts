4dcd553c3182d54b9bb37b2f8a96890f6df20aa5
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-03-27 15:35:47 -0300

    refactor: make TaskRepository extends Repository class

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,4 +1,4 @@
-import { DeleteResult, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Task } from './task.entity';
 import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -7,37 +7,33 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
-export class TaskRepository {
+export class TaskRepository extends Repository<Task> {
   constructor(
-    @InjectRepository(Task) private readonly repository: Repository<Task>,
-  ) {}
+    @InjectRepository(Task)
+    private readonly repository: Repository<Task>,
+  ) {
+    super(repository.target, repository.manager, repository.queryRunner);
+  }
+
   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
     const { title, description } = createTaskDto;
 
-    const task = this.repository.create();
+    const task = this.create();
     task.title = title;
     task.description = description;
     task.status = TaskStatus.OPEN;
-    await this.repository.save(task);
+    await this.save(task);
 
     return task;
   }
 
-  async save(task: Task): Promise<Task> {
-    return this.repository.save(task);
-  }
-
   async findById(id: number): Promise<Task> {
-    return this.repository.findOneBy({ id });
-  }
-
-  async remove(task: Task): Promise<DeleteResult> {
-    return this.repository.delete(task.id);
+    return this.findOneBy({ id });
   }
 
   async getTasks(filter: GetTasksFilterDto) {
     const { status, search } = filter;
-    const query = this.repository.createQueryBuilder('task');
+    const query = this.createQueryBuilder('task');
 
     if (status) {
       query.andWhere('task.status = :status', { status });
